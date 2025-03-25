c05a1e3ab7ca11c5b3bb5ae71788e3c10ac93383
Ehay2 <123690885+Ehay2@users.noreply.github.com>
2025-03-25 18:04:16 -0400

    Create writeback_stage.sv

---
+++ hdl/writeback_stage.sv
@@ -0,0 +1,26 @@
+module writeback_stage 
+import rv32i_types::*;
+(
+    //input  logic        clk,
+    input  logic        rst,
+    input  mem_wb_stage_reg_t mem_wb_reg,
+    output logic [4:0]      rd,
+    output logic [31:0]     rd_data,
+    output logic            regf_we
+);
+
+
+    always_comb begin
+        rd = '0;
+        rd_data = '0;
+        regf_we = 1'b0;
+
+        if (mem_wb_reg.valid && !rst) begin
+            rd = mem_wb_reg.rd;
+            rd_data = mem_wb_reg.rd_data;
+            regf_we = mem_wb_reg.regf_we;
+        end
+    end
+
+
+endmodule
