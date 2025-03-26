1acf57e257026f2f28033e2ab33442bfb5881518
Ehay2 <123690885+Ehay2@users.noreply.github.com>
2025-03-26 19:00:00 -0400

    Update cpu.sv

--- hdl/cpu.sv
+++ hdl/cpu.sv
@@ -14,6 +14,13 @@ module cpu
     input   logic               bmem_rvalid
 );
 
+    logic [31:0] pc, pc_next, next, pc_branch, prev_pc;
+    logic   inst_cache_hit;
+
+    // Instr Queue
+    logic full_o;
+    logic [31:0] data_i, data_o;
+
     deserializer cache_line_adapter (
         .clk        (clk),
         .rst        (rst),
@@ -32,7 +39,7 @@ module cpu
         .clk(clk),
         .rst (rst),
         
-        .ufp_addr(ufp_addr),
+        .ufp_addr(pc),  //.ufp_addr(ufp_addr),
         .ufp_rmask(ufp_rmask),
         .ufp_wmask(ufp_wmask),
         .ufp_rdata(ufp_rdata),
@@ -46,6 +53,25 @@ module cpu
         .dfp_wdata(dfp_wdata),
         .dfp_resp(dfp_resp)
     );
+
+
+    cache data_cache (
+        .clk        (clk),
+        .rst        (rst),
+        .ufp_addr   (),
+        .ufp_rmask  (),
+        .ufp_wmask  (),
+        .ufp_rdata  (),
+        .ufp_wdata  (),
+        .ufp_resp   (),
+        .dfp_addr   (),
+        .dfp_read   (),
+        .dfp_write  (),
+        .dfp_rdata  (),
+        .dfp_wdata  (),
+        .dfp_resp   ()
+    );
+
     localparam WIDTH = 32;
     localparam DEPTH = 32;
     localparam LEN = 32;
@@ -68,6 +94,7 @@ module cpu
     assign bmem_write = 0;
     assign bmem_wdata = 0;
 
+
     logic [255:0] line_buffer_i;
     logic line_buffer_valid;
     logic [255:0] line_buffer_o;
@@ -82,6 +109,59 @@ module cpu
         .data_o(line_buffer_o)
     );
 
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            pc    <= 32'haaaaa000;
+            order <= '0;
+        end else if (ufp_resp && ufp_rmask && !full_o) begin   // fetch
+            data_i = ufp_rdata;
+            enqueue_i = 1'b1;
+            pc_next = pc_next + 'd4;
+        end
+        else begin
+            pc <= pc_next;
+            if (commit)
+                order <= order + 'd1;
+        end
+    end
+    
 
+
+    localparam RS_DEPTH = 3;
+    localparam RS_ALU_WIDTH = 0; // valid(1) + inst decoding() + rs1(32) + rs1_ready(1) + rs2/imm(32) + rs2_ready(1) + rd_paddr(6?)
+    queue rs_alu #(
+        WIDTH = 
+    )(
+        .clk(clk),
+        .rst(rst),
+        .data_i(rs_alu_input),
+        .enqueue_i(rs_alu_enqueue),
+        .full_o(rs_alu_full),
+        .data_o(rs_alu_output),
+        .dequeue_i(rs_alu_dequeue),
+        .empty_o(rs_alu_empty)
+    );
+
+    queue rs_br (
+        .clk(clk),
+        .rst(rst),
+        .data_i(rs_br_input),
+        .enqueue_i(rs_br_enqueue),
+        .full_o(rs_br_full),
+        .data_o(rs_br_output),
+        .dequeue_i(rs_br_dequeue),
+        .empty_o(rs_br_empty)
+    );
+
+    queue rs_mem (
+        .clk(clk),
+        .rst(rst),
+        .data_i(rs_mem_input),
+        .enqueue_i(rs_mem_enqueue),
+        .full_o(rs_mem_full),
+        .data_o(rs_mem_output),
+        .dequeue_i(rs_mem_dequeue),
+        .empty_o(rs_mem_empty)
+    );
 
 endmodule : cpu
