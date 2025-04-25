4fde1f73ed8570d9928c269b79850a7bfa9e1da7
sujins2 <shin369123@gmail.com>
2025-04-24 20:16:44 -0500

    update mem linebuffer at store

--- mp_ooo/hdl/cpu.sv
+++ mp_ooo/hdl/cpu.sv
@@ -526,25 +526,23 @@ import rv32i_types::*;
         ufp_addr_mem = dmem_addr;
         ufp_rmask_mem = dmem_rmask;
         ufp_wmask_mem = dmem_wmask;
-        ufp_wdata_mem = dmem_wdata;
+        // ufp_wdata_mem = dmem_wdata;
 
-        // dmem_rdata = ufp_rdata_mem >> 4*dmem_addr[1:0];
+        ufp_wdata_mem = '0;
+        if (dmem_wmask[3])  ufp_wdata_mem[31:24] = dmem_wdata[31:24];
+        if (dmem_wmask[2])  ufp_wdata_mem[23:16] = dmem_wdata[23:16];
+        if (dmem_wmask[1])  ufp_wdata_mem[15:8] = dmem_wdata[15:8];
+        if (dmem_wmask[0])  ufp_wdata_mem[7:0] = dmem_wdata[7:0];
+
         dmem_rdata = '0;
-        if (dmem_rmask[3]) 
-            dmem_rdata[31:24] = ufp_rdata_mem[31:24];
-        
-        if (dmem_rmask[2]) 
-            dmem_rdata[23:16] = ufp_rdata_mem[23:16];
-
-        if (dmem_rmask[1]) 
-            dmem_rdata[15:8] = ufp_rdata_mem[15:8];
-
-        if (dmem_rmask[0]) 
-            dmem_rdata[7:0] = ufp_rdata_mem[7:0];
+        if (dmem_rmask[3])  dmem_rdata[31:24] = ufp_rdata_mem[31:24];
+        if (dmem_rmask[2])  dmem_rdata[23:16] = ufp_rdata_mem[23:16];
+        if (dmem_rmask[1])  dmem_rdata[15:8] = ufp_rdata_mem[15:8];
+        if (dmem_rmask[0])  dmem_rdata[7:0] = ufp_rdata_mem[7:0];
 
         dmem_resp = ufp_resp_mem;
         
-        if (mem_use_linebuffer) begin
+        if (|dmem_rmask && mem_use_linebuffer) begin
             ufp_addr_mem = '0;
             ufp_rmask_mem = '0;
             ufp_wmask_mem = '0;
@@ -584,6 +582,10 @@ import rv32i_types::*;
             curr_instr_addr = pc;
             curr_instr_data = '0;
             instr_enable = 1'b1;            
+        end else if (ufp_resp_mem && mem_stall && |dmem_wmask) begin
+            curr_dmem_addr = dmem_addr;
+            curr_dmem_data = ufp_wdata_mem;
+            dmem_enable = 1'b1;            
         end else if (ufp_resp_mem && mem_stall) begin
             curr_dmem_addr = dmem_addr;
             curr_dmem_data = ufp_rcache_line_mem;
